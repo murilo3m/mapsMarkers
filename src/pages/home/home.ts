d4cd8fb5d53c2d9bcca306475334662e5fd8043d
Murilo Maldonado Mendes <murilomaldonadom@gmail.com>
2018-05-08 17:07:42 -0300

    First commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,68 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+import { GoogleMaps, GoogleMap, GoogleMapsEvent, GoogleMapOptions } from '@ionic-native/google-maps';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	@ViewChild('map')
+	private mapElement: ElementRef;
+	private map: GoogleMap;
 
-  }
+	constructor(public navCtrl: NavController, public platform: Platform) {
 
+	}
+
+	ionViewDidLoad() {
+		console.log('ok');
+		this.platform.ready().then(() => {
+			console.log('ok2');
+			this.loadMap();
+		});
+	}
+
+	loadMap() {
+		let mapOptions: GoogleMapOptions = {
+			camera: {
+				target: {
+					lat: -20.5359701,
+					lng: -47.3906893
+				},
+				zoom: 13
+			}
+		};
+
+		let element = this.mapElement.nativeElement;
+		console.log(element);
+		this.map = GoogleMaps.create(element, mapOptions);
+		console.log(this.map);
+		//Get Geolocations
+		this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
+			console.log('ready');
+			let name = 'This is a test';
+			let address = 'Rua Nabi Haber';
+			let number_home = '465';
+			let neighborhood = 'São José';
+
+			let config = {
+				title: name,
+				snippet: address + ', ' + number_home + ' - ' + neighborhood,
+				animation: 'DROP',
+				flat: true,
+				position: {
+					lat: -20.5359701,
+					lng: -47.3906893
+				}
+			};
+
+			this.map.addMarker(config);
+
+		}, (err) => {
+			console.log('error');
+			console.log(err);
+		});
+	}
 }
